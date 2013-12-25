39a126db61590a3e4398f3108b4e47ef0627cb9d
Carl Alexander <carlalexander@gmail.com>
2013-12-25 02:35:53 -0500

    Ran php-cs-fix

--- Command/InstallCommand.php
+++ Command/InstallCommand.php
@@ -12,7 +12,6 @@
 namespace Helthe\Bundle\TurbolinksBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
