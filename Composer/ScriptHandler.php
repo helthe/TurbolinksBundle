59653525d7a10f3441c45a00b4000ff962297a2f
Carl Alexander <carlalexander@gmail.com>
2014-01-30 16:47:19 -0500

    Missing copyright

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Helthe Turbolinks package.
+ *
+ * (c) Carl Alexander <carlalexander@helthe.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Helthe\Bundle\TurbolinksBundle\Composer;
 
 use Symfony\Component\Process\Process;
